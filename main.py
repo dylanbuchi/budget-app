b584925e97ba5522632b1f24dc21b9a60777b891
Dylan Buchi <devybuchi@gmail.com>
2021-08-07 06:48:28 -0300

    update main.py

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
-import budget
+from budget import Category
 from budget import create_spend_chart
 from unittest import main
+from pprint import pprint
 
-food = budget.Category("Food")
-food.deposit(1000, "initial deposit")
-food.withdraw(10.15, "groceries")
-food.withdraw(15.89, "restaurant and more food for dessert")
-print(food.get_balance())
-clothing = budget.Category("Clothing")
-food.transfer(50, clothing)
-clothing.withdraw(25.55)
-clothing.withdraw(100)
-auto = budget.Category("Auto")
-auto.deposit(1000, "initial deposit")
-auto.withdraw(15)
+food = Category("Food")
+entertainment = Category("Entertainment")
+business = Category("Business")
 
-print(food)
-print(clothing)
+food.deposit(900, "deposit")
+entertainment.deposit(900, "deposit")
+business.deposit(900, "deposit")
+food.withdraw(105.55)
+entertainment.withdraw(33.40)
+business.withdraw(10.99)
 
-print(create_spend_chart([food, clothing, auto]))
+a = create_spend_chart([business, food, entertainment])
+
+print(create_spend_chart([business, food, entertainment]))
 
 # Run unit tests automatically
 # main(module='test', exit=False)
